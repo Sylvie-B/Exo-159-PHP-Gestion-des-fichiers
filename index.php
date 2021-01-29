2b20936ad3ad5333ae1870f996f87016967f7474
Sylvie-B <unsolo@hotmail.fr>
2021-01-29 12:47:02 +0100

    get content, fgets, fgetc, open / close

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
  * 2. Afficher le contenu du fichier dans un div
  */
 // TODO Votre code ici.
+$content = file_get_contents('lire.txt');
+if($content !== false){
+    echo $content;
+}
+echo "<br><br>";
 
 /**
  * 3. Faites la même chose à l'aide d'une boucle et de la fonction fgets(), récupérez les lignes du fichier une à une.
@@ -12,14 +17,25 @@
  * --> N'oubliez pas de fermer votre fichier.
  */
 // TODO Votre code ici.
-
-
+$file = fopen('lire.txt', 'rb');
+while($ligne = fgets($file)) {
+    echo nl2br($ligne);
+}
+fclose($file);
+echo "<br><br>";
 /**
  * 5. Récupérez les caractères un à un et n'affichez le caractère que s'il s'agit d'autre chose qu'un espace, qu'un point ou qu'un point d'interrogation / exclamation.
  * --> N'oubliez pas de fermer votre fichier.
  */
 // TODO Votre code ici.
+$file = fopen('lire.txt', 'rb');
+while ($char = fgetc($file)){
+    if($char !== " " && $char !== "." && $char !== "," && $char !== "?" && $char !== "!"){
+        echo $char;
+    }
 
+}
+fclose($file);
 
 /**
  * 6. Ne cherchez pas la logique dans cet exo ^^
